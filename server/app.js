4e1d49ce00522cd119905645b4be77691abaa87c
OmarAyman85 <oamr.shivy85@gmail.com>
2024-12-24 16:19:04 +0200

    Rendering the creation form and creation of a new one

--- server/app.js
+++ server/app.js
@@ -19,15 +19,30 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// General request
 app.get("/", (req, res) => {
   res.render("home");
 });
-
+// Getting all the campgrounds
 app.get("/campgrounds", async (req, res) => {
   const campgrounds = await Campground.find({});
   res.render("campgrounds/index", { campgrounds });
 });
-
+// Request for creating a new campground
+// Reordered here before the getting by the ID because /new will be treated as an id not a route
+app.get("/campgrounds/new", async (req, res) => {
+  res.render("campgrounds/new");
+});
+// Creating new campground after submitting the form
+app.post("/campgrounds", async (req, res) => {
+  const campground = new Campground(req.body.campgrounds);
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`);
+});
+// Getting details of specific campground
 app.get("/campgrounds/:id", async (req, res) => {
   const campground = await Campground.findById(req.params.id);
   res.render("campgrounds/show", { campground });
