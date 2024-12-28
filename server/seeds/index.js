ca90d6bc910b977acb6dc3bafb2a52861a5644f5
OmarAyman85 <oamr.shivy85@gmail.com>
2024-12-28 05:17:37 +0200

    Adding Images

--- server/seeds/index.js
+++ server/seeds/index.js
@@ -19,9 +19,15 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000) + 1;
+    const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       location: `${cities[random1000].city},${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      image:
+        "https://images.unsplash.com/photo-1563373983-04e07c359192?q=80&w=1788&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      description:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi veritatis neque quo iusto sequi sunt quos, minima cum, totam itaque voluptatem corporis voluptate eveniet commodi ea fugiat. Laboriosam, minima ab!",
+      price,
     });
     await camp.save();
   }
